957a22f6c70e5f99c7a7c7efa582054b01af18e0
nudel98@ya.ru <ameon@ya.ru>
2023-06-28 08:44:02 +0500

    update

--- Seminars/Sem3/Program.cs
+++ Seminars/Sem3/Program.cs
@@ -1,33 +1,40 @@
 ﻿// Задание1.
 // Напишите программу, которая принимает на вход координаты точки (X и Y),
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-int FintQuad (int x, int y)
-{
-  if(x > 0 && y > 0)
-  {
-    return 1;
-  }
+// int FintQuad (int x, int y)
+// {
+//   if(x > 0 && y > 0)
+//   {
+//     return 1;
+//   }
   
-  if(x < 0 && y > 0)
-  {
-    return 2;
-  }
+//   if(x < 0 && y > 0)
+//   {
+//     return 2;
+//   }
   
-  if(x < 0 && y < 0)
-  {
-    return 3;
-  }
+//   if(x < 0 && y < 0)
+//   {
+//     return 3;
+//   }
   
-  if(x > 0 && y < 0)
-  {
-    return 4;
-  }
-  return 0;
-}
+//   if(x > 0 && y < 0)
+//   {
+//     return 4;
+//   }
+//   return 0;
+// }
 
-System.Console.WriteLine("Input X coordinate: ");
-int x = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Input Y coordinate: ");
-int y = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input X coordinate: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input Y coordinate: ");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"Point with X coordinate ({x}) and Y coordinate ({y}) belongs to {FintQuad(x,y)} quart");
+// System.Console.WriteLine($"Point with X coordinate ({x}) and Y coordinate ({y}) belongs to {FintQuad(x,y)} quart");
+
+
+// Задача 3.
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
